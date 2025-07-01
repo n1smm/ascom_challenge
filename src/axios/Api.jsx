bf6427fd48a55a64bce56db076bd6e9bb8ea5ef4
n1smm <tjaz.juvan@gmail.com>
2025-07-01 16:11:07 +0200

    added env file

--- src/axios/Api.jsx
+++ src/axios/Api.jsx
@@ -1,19 +1,19 @@
 import axios from "axios";
+// import "dotenv/config";
+
 
 const api = axios.create({
 	baseURL: 'https://mobile.digistat.it/CandidateApi',
 	auth: {
-		username: 'test',
-		password: 'TestMePlease!'
+		username: import.meta.env.VITE_API_USER,
+		password: import.meta.env.VITE_API_PASS
 	}
 });
-
 
 const getPatients = async () => {
 		const response = await api.get('/Patient/GetList');
 		console.log(response.data);
 		return response.data;
 };
-
 
 export  {getPatients};
